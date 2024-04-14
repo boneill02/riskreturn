3516354050f27078e8e9217ed72ac5e39142e09f
Ben O'Neill <ben@oneill.sh>
2024-04-14 21:43:51 +0100

    Cleanup

--- riskreturn.js
+++ riskreturn.js
@@ -1,61 +1,80 @@
 /*!
- * @author Ben O'Neill <ben@benoneill.xyz>
- * @license
- * Copyright (c) 2021 Ben O'Neill <ben@benoneill.xyz>
+ * @author Ben O'Neill <ben@oneill.sh>
+ * @license GNU AGPL v3
+ * Copyright (c) 2021 Ben O'Neill <ben@oneill.sh>
  * Licensed under the GNU AGPL v3.
  */
 
 var user_begin = 0;
 var user_end = 0;
 var user_return = 0;
-
 var nasdaq_begin = 0;
 var nasdaq_end = 0;
 var nasdaq_return = 0;
+
+var form = document.getElementById('inform');
+var result = document.getElementById('result');
+var canvas = document.getElementById('myChart');
 
 var result_less = 'Your return is less than the index. Lengthen your investment time period to see if you can overcome the low/negative returns.';
 var result_more = 'Nice! You beat the Nasdaq!';
 var result_negative = 'Your return is negative! This is the downside of risk. Change your dates or lengthen your investment period to see what happens.';
 
+/**
+	Round to nearest hundredth
+*/
 function numfmt(num) {
-	/* round to nearest hundredth */
-	//var rounded = Math.round(num * 100) / 100
-	
 	return Intl.NumberFormat().format(parseFloat(num).toFixed(2));
-	/* add commas */
-	
 }
 
-function riskreturncalc() {
-	/* find date indexes */
-	var start_date_index;
-	var end_date_index;
-	for (var i = 0; i < monthly_data.length; i++) {
-		if (document.getElementById('sdate').value === monthly_data[i][0]) {
-			start_date_index = i;
-		}
-		if (document.getElementById('edate').value === monthly_data[i][0]) {
-			end_date_index = i;
-		}
-	}
+/*
+	Get the number of months in a given MM/YYYY date since year 0
+*/
+function getMonthNumber(s) {
+	var vals = s.split('/');
+	return (parseInt(vals[1]) * 12) + parseInt(vals[0]);
+}
 
-	if (start_date_index == undefined || end_date_index == undefined) {
+/**
+	Convert date from YYYY-MM to MM/YYYY
+*/
+function convertDate(s) {
+	var vals = s.split('-');
+	return vals[1] + '/' + vals[0];
+}
+
+/**
+	Get month in MM/YYYY n months after s
+*/
+function dateOffset(s, n) {
+	var vals = s.split('/');
+	var month = parseInt(vals[0]) + Math.floor(n % 12);
+	var year = parseInt(vals[1]) + Math.floor(n / 12);
+	
+	if (month < 10)
+		return '0' + month + '/' + year;
+
+	return month + '/' + year;
+}
+
+function riskReturn() {
+	var data = JSON.parse(ndata);
+	var sdate = convertDate(document.getElementById('sdate').value);
+	var edate = convertDate(document.getElementById('edate').value);
+	var monthly_in = Number(document.getElementById('ubegin').value);
+	var months = getMonthNumber(edate) - getMonthNumber(sdate);
+
+	if (sdate == null || edate == null || monthly_in == null) {
 		alert("Form must be fully completed");
 		return;
 	}
-	if (start_date_index >= end_date_index) {
+	if (months <= 0) {
 		alert("End date must be after start date");
 		return;
 	}
 
-	user_begin = Number(document.getElementById('ubegin').value);
-
-	var monthly_in = user_begin;
-
-	var months = end_date_index - start_date_index;
-
-	var start_data = monthly_data[start_date_index];
-	var end_data = monthly_data[start_date_index];
+	var start_data = data[sdate];
+	var end_data = data[edate];
 	var total_invested = monthly_in * months;
 	var value_by_month = [];
 	var nasdaq_value_by_month = [];
@@ -64,21 +83,22 @@ function riskreturncalc() {
 	/* calculate cumulative shares */
 	var shares_purchased = 0;
 	var cumulative_shares = 0;
-	for (var i = start_date_index; i <= end_date_index; i++) {
-		shares_purchased = (monthly_in / (monthly_data[i][1]));
+	for (var i = 0; i <= months; i++) {
+		var idx = dateOffset(sdate, i);
+		console.log(idx);
+		shares_purchased = (monthly_in / (data[idx][0]));
 		cumulative_shares += shares_purchased;
-		value_by_month.push(cumulative_shares * monthly_data[i][1]);
-		nasdaq_value_by_month.push(monthly_data[i][2]);
+		value_by_month.push(cumulative_shares * data[idx][0]);
+		nasdaq_value_by_month.push(data[idx][1]);
 
 		/* for chart */
-		labels.push(monthly_data[i][0]);
+		labels.push(idx);
 	}
 
-	user_end = cumulative_shares * monthly_data[end_date_index][1];
-	user_return = RATE(months, (monthly_in * -1), 0, user_end) * 12;
-
-	nasdaq_begin = monthly_data[start_date_index][2];
-	nasdaq_end = monthly_data[end_date_index][2];
+	user_end = cumulative_shares * end_data[0];
+	user_return = RATE(months, (monthly_in * -1), 0, edate) * 12;
+	nasdaq_begin = start_data[1];
+	nasdaq_end = end_data[1];
 	nasdaq_return = RATE(months, 0, (nasdaq_begin * -1), nasdaq_end) * 12;
 
 	/* display results */
@@ -97,18 +117,8 @@ function riskreturncalc() {
 		document.getElementById("message").innerHTML = result_negative;
 	}
 
-	/* make chart */
-	var old_graph = document.getElementById('graph');
-	if (old_graph != null) {
-		document.body.removeChild(old_graph);
-	}
-
-	var ctx = document.createElement('canvas');
-	document.body.appendChild(ctx);
-	ctx.id = 'graph';
-	ctx.width = 400;
-	ctx.height = 400;
-	const data = {
+	var ctx = canvas.getContext("2d");
+	const chartData = {
 		labels: labels,
 		datasets: [{
 			axis: 'y',
@@ -141,7 +151,7 @@ function riskreturncalc() {
 
 	var graph = new Chart(ctx, {
 		type: 'line',
-		data: data,
+		data: chartData,
 		options: {
 			scales: {
 				'user_value': {
